63e8f2c37be72c00058dec6f58804541603757a4
rick-kenti <erickmakenti@gmail.com>
2025-09-20 13:37:30 +0300

    Declare customerName in global scope and initialize also declared leastfavoritecustomer as a const

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+// Declare customerName in global scope and initialize to 'bob'
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'; // This will create a global variable
+}
+
+function overwriteBestCustomer(newBestCustomer) {
+    bestCustomer = newBestCustomer;
+}
+
+// Declare leastFavoriteCustomer as a constant
+const leastFavoriteCustomer = 'someone';
+
+function changeLeastFavoriteCustomer() {
+    // This will throw an error because we're trying to reassign a constant
+    leastFavoriteCustomer = 'someone else';
+}
